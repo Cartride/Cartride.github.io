7c5c51f82090c3c2092ed0524573a8748280b46b
mcfon <emcfon@gmail.com>
2021-05-24 11:22:11 +0800

    Site updated: 2021-05-24 11:22:10

--- js/main.js
+++ js/main.js
@@ -115,6 +115,7 @@
 
     const $toc = $('#toc');
     if ($toc.length > 0) {
+        $toc.addClass('column-left is-sticky');
         const $mask = $('<div>');
         $mask.attr('id', 'toc-mask');
 
